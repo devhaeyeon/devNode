4490475992a6dcbde033d94a362242833a8c06e1
kimhaeyeon <data_mwhy@naver.com>
2013-10-21 09:45:31 +0900

    [add] mongodb dependency
    mongose db dependency add

--- web.js
+++ web.js
@@ -43,5 +43,5 @@ io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
-    });
+    }); 
 });
