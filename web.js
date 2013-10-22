536ee8eb871ee3e1a8a496aa7cd8ed9ddddccd47
kimhaeyeon <data_mwhy@naver.com>
2013-10-22 18:21:02 +0900

    [modify] ejs modify
    ejs modify

--- web.js
+++ web.js
@@ -102,7 +102,7 @@ http.createServer(function (request,response){
 
 var ejs = require('ejs')
   , fs = require('fs')
-  , path = __dirname + '/functions.ejs'
+  , path = __dirname + 'functions.ejs'
   , str = fs.readFileSync(path, 'utf8');
 
 var users = [];
