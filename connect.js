79697e8123e55d52c997ea79442f49e00b2433cc
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 13:38:17 +0900

    [modify] error handler, use
    error handler, use

--- connect.js
+++ connect.js
@@ -1,12 +1,33 @@
 var connect=require('connect');
 
 var port = process.env.PORT || 5000;
+var server = connect.createServer();
 
-connect.createServer(connect.query(),function (request,response){
+
+server.use(connect.query());
+server.use(function (request,response){
+
+		response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+		response.end( '<h1>hello, connect module'+JSON.stringify(request.query)+'</h1>');
+
+});
+
+server.use(connect.errorHandler({
+
+	stack:true;
+	message:true;
+	dump:true;
+
+}));
+
+
+/*connect.createServer(connect.query(),function (request,response){
 
 		response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
 		response.end( '<h1>hello, connect module'+JSON.stringify(request.query)+'</h1>');
 
 }).listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
+
+
