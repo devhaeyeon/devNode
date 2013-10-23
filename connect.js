f78628f9a7da0213ec68488e2b47083dbe428d5f
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 13:25:07 +0900

    [modify] json query
    json query

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 5000;
 connect.createServer(connect.query(),function (request,response){
 
 		response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-		response.end( '<h1>hello, connect module</h1>');
+		response.end( '<h1>hello, connect module'+JSON.stringify(request.query)+'</h1>');
 
 }).listen(port, function() {
   console.log("Listening on " + port);
