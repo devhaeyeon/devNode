384b0a852938fa9df0b0a977a414a55c0f2c44a2
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 10:56:20 +0900

    [modify] jade
    jade modify

--- web.js
+++ web.js
@@ -126,6 +126,22 @@ http.createServer(function (request,response){
 });
 */
 
+var http			=		require('http');
+var ejs			=		require('jade');
+var fs				=		require('fs');
+var port = process.env.PORT || 5000;
+
+http.createServer(function (request,response){
+
+		fs.readFile('jadepage.jade','utf-8',function(error,data){
+		var fn=jade.compile(data);
+		response.writeHead(200, {'Content-Type':'text/html'});
+		response.end( fn());
+		});
+
+}).listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 
 
