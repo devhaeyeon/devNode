62ee0ff067be9bb1cdf24e923560deb27c981b22
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 16:10:25 +0900

    [modify] route modify
    route modify

--- route.js
+++ route.js
@@ -1,19 +1,27 @@
-var connect = require('connect');
-var rServer = connect.createServer();
  var port = process.env.PORT || 5000;
-
-rServer.use('/Home/Index',function(request,response,next){
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.write('<h1>Index Page</h1>');
-    response.end();
-});
- 
-rServer.use('/Home/About',function(request,response,next){
-    response.writeHead(200,{'Content-Type':'text/html'});
-    response.write('<h1>Index Page</h1>');
-    response.end();
-});
- 
-rServer.listen(port, function() {
+// 모듈을 추출합니다.
+var connect = require('connect');
+// 서버를 생성합니다.
+connect.createServer(connect.router(function (app) {
+    
+// GET - /Home/Index
+    app.get('/Home/Index', function (request, response, next) {
+        
+// 응답합니다.
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<h1>Index Page</h1>');
+        response.end();
+    });
+    
+// GET - /Home/About
+    app.get('/Home/About', function (request, response, next) {
+        
+// 응답합니다.
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<h1>About Page</h1>
+<p>');
+        response.end();
+    });
+})).listen(port, function() {
   console.log("Listening on " + port);
 });
