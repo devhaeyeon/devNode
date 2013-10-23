eb71be49756271851a0fc7e73546a5a4bb54c18c
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 15:48:04 +0900

    [modify] routes.js
    routes.js

--- route.js
+++ route.js
@@ -5,12 +5,8 @@ connect.createServer(connect.router(function (app) {
 
 	app.get('/Product/:id'	,function (request,response,next){
 		response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-		response.write( '<h1>product page-'+request.params.id+'</h1>');
-		response.end();
-	});
-
-	app.all('*'	,function (request,response,next){
-		throw new Error('page not found');
+		response.end( '<h1>product page-'+request.params.id+'</h1>');
+		//response.end();
 	});
 
 })).listen(port, function() {
