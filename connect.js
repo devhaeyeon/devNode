e3eedf68fcee6c329f9411eb484e6942ee74d24e
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 13:21:53 +0900

    [modify] connect.js
    query

--- connect.js
+++ connect.js
@@ -1,18 +1,11 @@
-var http			=		require('http');
-var jade			=		require('jade');
-var fs				=		require('fs');
+var connect=require('connect');
+
 var port = process.env.PORT || 5000;
 
-http.createServer(function (request,response){
+connect.createServer(connect.query(),function (request,response){
 
-		fs.readFile('jadepage.jade','utf-8',function(error,data){
-		var fn=jade.compile(data);
 		response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-		response.end( fn({
-				name : 'haeyeon',
-				description:'해연22'
-		}));
-		});
+		response.end( '<h1>hello, connect module</h1>');
 
 }).listen(port, function() {
   console.log("Listening on " + port);
