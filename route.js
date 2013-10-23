6063823ce3e183268581df3b624a800e85cb5319
kimhaeyeon <data_mwhy@naver.com>
2013-10-23 15:54:19 +0900

    [modify] route modify
    route modify

--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var rServer = connect.createServer();
- 
+ var port = process.env.PORT || 5000;
+
 rServer.use('/Home/Index',function(request,response,next){
     response.writeHead(200,{'Content-Type':'text/html'});
     response.write('<h1>Index Page</h1>');
