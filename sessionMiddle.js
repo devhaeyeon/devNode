ba0b980987344acfa3388575034c54d8d590c2c8
kimhaeyeon <data_mwhy@naver.com>
2013-10-24 13:36:51 +0900

    [modify] session middleware
    session middleware

--- sessionMiddle.js
+++ sessionMiddle.js
@@ -1,26 +1,24 @@
-var connect=require('connect');
-var server=connect.createServer();
 var port = process.env.PORT || 5000;
 
+// 모듈을 추출합니다.
+var connect = require('connect');
+// 서버를 생성합니다.
+var server = connect.createServer();
+// 미들웨어를 사용합니다.
 server.use(connect.cookieParser());
 server.use(connect.session());
-server.use(function (request,response){
-
-	var output='';
-	output +='<p>cookies:'+JSON.stringify(request.cookies)+'</p>';
-	output +='<h1>cookies:'+JSON.stringify(request.session)+'</h1>';
-
-	request.session.now=(new Date()).toUTCString();
-
-	response.writeHead(200, {'Content-Type':'text/html'});
-	response.end(output);
-
-
+server.use(function (request, response) {
+    // 변수를 선언합니다.
+    var output = '';
+    output += '<h1>Cookies: ' + JSON.stringify(request.cookies) + '</h1>';
+    output += '<h1>Session: ' + JSON.stringify(request.session) + '</h1>';
+    // 응답합니다.
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(output);
+    // 세션을 저장합니다.
+    response.session.now = (new Date()).toUTCString();
 });
-
-server.listen(port,function(){
-
-	console.log('server running '+port);
-
-
+// 서버를 실행합니다.
+server.listen(port, function () {
+    console.log('server running at'+port);
 });
