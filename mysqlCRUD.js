f01e5817b88f22e4c21e1c21e174bbaeb7ff77ad
kimhaeyeon <data_mwhy@naver.com>
2013-10-25 15:32:08 +0900

    [mdify] 수정
    수정

--- mysqlCRUD.js
+++ mysqlCRUD.js
@@ -2,16 +2,13 @@ var express = require('express');
 var mysql      = require('mysql');
 var app = express();
 app.use(express.logger());
-
 var connection = mysql.createConnection({
   host     : 'us-cdbr-east-04.cleardb.com', //변하지 않음.
   user     : 'b30e9317bebc55',
   password : '56bd55a0',
   database : 'heroku_e4b18fa76539562'
 });
-
 connection.connect();
-
 app.get('/', function(request, response) {
   connection.query('SELECT * from t_users', function(err, rows, fields) {
       if (err) {
