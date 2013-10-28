4c0544e17c46ad1e6e2ba6444b5a357d7bfc0d67
kimhaeyeon <data_mwhy@naver.com>
2013-10-28 13:47:48 +0900

    [add]mongodb board
    mongodb board

---
+++ mongoBoard.js
@@ -0,0 +1,26 @@
+var connect = require('connect');
+var fs = require('fs');
+var ejs = require('ejs');
+var port = process.env.PORT || 5000;
+var mongodb = require('mongodb')
+  , MongoClient = mongodb.MongoClient;
+
+var db = mongodb.connect('app18606745', 'test');
+
+function get(path, cd) {
+    return function (req, res, next) {
+        if (req.method != 'GET' || req.url != path)
+            return next();
+        cd(req, res, next);
+    }
+}
+var app = connect()
+    .use(get('/', function (req, res, next) {
+        fs.readFile('input.html', 'utf8', function (error, data) {
+            db.haeyeon.find({}, { name: 1 }, function (error, cursor) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(ejs.render(data, { data: cursor }));
+            });
+        });
+    })).listen(port, function () { console.log("서버 연결 : 3000") });
+ 
