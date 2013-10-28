d2706f69e9095aec468f0fb8a6ae4010c3f8d52c
kimhaeyeon <data_mwhy@naver.com>
2013-10-28 14:52:50 +0900

    [modify] mongos db test
    test

--- mongoBoard.js
+++ mongoBoard.js
@@ -1,33 +1,24 @@
-var mongodb = require('mongodb')
-  , MongoClient = mongodb.MongoClient
- 
-MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
-  // operate on the collection named "test"
-  var collection = db.collection('test')
- 
-  // remove all records in collection (if any)
-  console.log('removing documents...')
-  collection.remove(function(err, result) {
-    if (err) {
-      return console.error(err)
+var connect = require('connect');
+var fs = require('fs');
+var ejs = require('ejs');
+var db = require('mongojs').connect(process.env.MONGOHQ_URL, ['test']);
+
+function get(path, cb){
+    return function(req, res ,next){
+     if(req.method != 'GET' || req.url != path) return next();
+        cb(req, res, next);
     }
-    console.log('collection cleared!')
-    // insert two documents
-    console.log('inserting new documents...')
-    collection.insert([{name: 'tester22'}, {name: 'coder222'}], function(err,
-docs) {
-      if (err) {
-        return console.error(err)
-      }
-      console.log('just inserted ', docs.length, ' new documents!')
-      collection.find({}).toArray(function(err, docs) {
-        if (err) {
-          return console.error(err)
-        }
-        docs.forEach(function(doc) {
-          console.log('found document: ', doc)
-        })
-      })
-    })
-  })
-})
+}
+
+var app = connect()
+    .use(get('/', function (req, res, next){
+        fs.readFile('test.htm', 'utf8', function(error, data){
+            db.test.find({},  {name:1, score:1, _id:0}, function (error, cursor) { 
+                res.writeHead(200, {'Content-Type':'text/html'});
+                res.end(ejs.render(data, {
+                    data : cursor
+                })); 
+            });
+        });
+    }))
+    .listen(8080, function(){console.log("server start 127.0.0.1:8080");});
