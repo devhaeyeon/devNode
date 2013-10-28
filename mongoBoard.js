86d7b380cc4b7209065b83cb0bf96b8c4d61d466
kimhaeyeon <data_mwhy@naver.com>
2013-10-28 15:02:00 +0900

    [modify] mongodb select
    mongodb select

--- mongoBoard.js
+++ mongoBoard.js
@@ -2,6 +2,8 @@ var connect = require('connect');
 var fs = require('fs');
 var ejs = require('ejs');
 var db = require('mongojs').connect(process.env.MONGOHQ_URL, ['test']);
+var port = process.env.PORT || 5000;
+
 
 function get(path, cb){
     return function(req, res ,next){
@@ -13,7 +15,7 @@ function get(path, cb){
 var app = connect()
     .use(get('/', function (req, res, next){
         fs.readFile('test.htm', 'utf8', function(error, data){
-            db.test.find({},  {name:1, score:1, _id:0}, function (error, cursor) { 
+            db.test.find({}, function (error, cursor) { 
                 res.writeHead(200, {'Content-Type':'text/html'});
                 res.end(ejs.render(data, {
                     data : cursor
@@ -21,4 +23,4 @@ var app = connect()
             });
         });
     }))
-    .listen(8080, function(){console.log("server start 127.0.0.1:8080");});
+    .listen(port, function(){console.log("server start 127.0.0.1:"+port);});
