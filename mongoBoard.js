81c4df8add17da69be9b0e0d72e9203a61d1d2b8
kimhaeyeon <data_mwhy@naver.com>
2013-10-28 14:13:17 +0900

    [modify] db test
    db test

--- mongoBoard.js
+++ mongoBoard.js
@@ -1,26 +1,33 @@
-var connect = require('connect');
-var fs = require('fs');
-var ejs = require('ejs');
-var port = process.env.PORT || 5000;
 var mongodb = require('mongodb')
-  , MongoClient = mongodb.MongoClient;
-
-var db = mongodb.connect('app18606745', 'test');
-
-function get(path, cd) {
-    return function (req, res, next) {
-        if (req.method != 'GET' || req.url != path)
-            return next();
-        cd(req, res, next);
-    }
-}
-var app = connect()
-    .use(get('/', function (req, res, next) {
-        fs.readFile('input.html', 'utf8', function (error, data) {
-            db.test.find({}, { name: 1 }, function (error, cursor) {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(ejs.render(data, { data: cursor }));
-            });
-        });
-    })).listen(port, function () { console.log("서버 연결 : 3000") });
+  , MongoClient = mongodb.MongoClient
  
+MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
+  // operate on the collection named "test"
+  var collection = db.collection('test')
+ 
+  // remove all records in collection (if any)
+  console.log('removing documents...')
+  collection.remove(function(err, result) {
+    if (err) {
+      return console.error(err)
+    }
+    console.log('collection cleared!')
+    // insert two documents
+    console.log('inserting new documents...')
+    collection.insert([{name: 'tester'}, {name: 'coder'}], function(err,
+docs) {
+      if (err) {
+        return console.error(err)
+      }
+      console.log('just inserted ', docs.length, ' new documents!')
+      collection.find({}).toArray(function(err, docs) {
+        if (err) {
+          return console.error(err)
+        }
+        docs.forEach(function(doc) {
+          console.log('found document: ', doc)
+        })
+      })
+    })
+  })
+})
